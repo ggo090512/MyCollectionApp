e9eebb97bd10c560db95037af42ecb3d3a8ac618
MinhHieu <hieumd090500@gmail.com>
2022-01-09 21:29:31 +0700

    Update and Delete Category

--- app/src/main/java/com/example/MyCollection/Acitvity/HomeActivity.java
+++ app/src/main/java/com/example/MyCollection/Acitvity/HomeActivity.java
@@ -13,8 +13,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -32,6 +38,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.MyCollection.Interface.ItemClickListener;
+import com.example.MyCollection.Models.ImageItem;
 import com.example.MyCollection.ViewHolder.MenuViewHolder;
 import com.example.MyCollection.Common.Container;
 import com.example.MyCollection.Models.Category;
@@ -40,6 +47,7 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -47,16 +55,22 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.normal.TedPermission;
+import com.rengwuxian.materialedittext.MaterialEditText;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.List;
 
 import gun0912.tedbottompicker.TedBottomPicker;
+import info.hoang8f.widget.FButton;
 import io.paperdb.Paper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -66,15 +80,23 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     public static DatabaseReference databaseReference;
     public static FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter;
 
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReferenceFromUrl("gs://foodnews-ea4e2.appspot.com");
+
     public static RecyclerView recyclerview;
     public static RecyclerView.LayoutManager layoutManager;
     public static File file;
+
     public static CardView cardView;
+    public static FButton fUpdate, fSelete,btnTest;
+    public static MaterialEditText edtName;
+
     String loginId = "";
+
     FloatingActionButton fab;
     TextView nameUser;
     FirebaseAuth fAuth;
-    ImageView imagepicker;
+    ImageView imageUpdate;
     public static String categorID;
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -99,7 +121,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         nameUser = (TextView)hView.findViewById(R.id.name_User);
         fAuth = FirebaseAuth.getInstance();
         FirebaseUser user = fAuth.getCurrentUser();
-        //imagepicker = findViewById(R.id.imageViewCreateCata);
+
         String[] name =user.getEmail().split("@");
         nameUser.setText(name[0]);
 
@@ -134,47 +156,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         }
         loadMenu2();
     }
-
-//    private void requestPermission(){
-//        PermissionListener permissionlistener = new PermissionListener() {
-//            @Override
-//            public void onPermissionGranted() {
-//                Toast.makeText(HomeActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
-//                openImagePicker();
-//            }
-//
-//            @Override
-//            public void onPermissionDenied(List<String> deniedPermissions) {
-//                Toast.makeText(HomeActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
-//            }
-//        };
-//        TedPermission.create()
-//                .setPermissionListener(permissionlistener)
-//                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
-//                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                .check();
-//    }
-//
-//    private void openImagePicker(){
-//        TedBottomPicker.OnImageSelectedListener imageSelectedListener = new TedBottomPicker.OnImageSelectedListener(){
-//
-//            @Override
-//            public void onImageSelected(Uri uri) {
-//                try {
-//                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-//                    imagepicker.setImageBitmap(bitmap);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        };
-//        TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(HomeActivity.this)
-//                .setOnImageSelectedListener(imageSelectedListener)
-//                .create();
-//        tedBottomPicker.show(getSupportFragmentManager());
-//
-//    }
-
 
     public void LoadMenu(String loginID){
 
@@ -212,14 +193,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 MenuViewHolder.setOnClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        //Toast.makeText(HomeActivity.this, ""+ adapter.getRef(position).getKey() ,Toast.LENGTH_SHORT).show();
-
                         Intent foodIntent = new Intent(HomeActivity.this, ViewImageActivity.class);
                         foodIntent.putExtra("CategoryId", adapter.getRef(position).getKey());
                         categorID = adapter.getRef(position).getKey();
                         startActivity(foodIntent);
-                        //categoryId = getIntent().getStringExtra("CategoryId");
-                        Log.d("home", categorID+"");
                     }
                 });
             }
@@ -267,9 +244,160 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
         DrawerLayout drawable = findViewById(R.id.drawer_layout);
         drawable.closeDrawer(GravityCompat.START);
         return false;
     }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if(item.getTitle().equals(Container.UPDATE)){
+            showDialogUpdate( adapter.getRef(item.getOrder()).getKey(), adapter.getItem(item.getOrder()));
+        }else if(item.getTitle().equals(Container.DELETE)){
+            deleteCategory( adapter.getRef(item.getOrder()).getKey());
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteCategory(String key) {
+        databaseReference = FirebaseDatabase.getInstance().getReference(Container.CATEGORY);
+        databaseReference.child(key).removeValue();
+    }
+
+    private void showDialogUpdate(String key, Category cate) {
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(HomeActivity.this);
+        dialog.setTitle("UPDATE CATEGORY");
+        dialog.setMessage("Chưa biết ghi gì");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View addMenu = inflater.inflate(R.layout.add_category, null);
+
+        fUpdate = addMenu.findViewById(R.id.btnUpdate);
+        fSelete = addMenu.findViewById(R.id.btnSelect);
+        edtName = addMenu.findViewById(R.id.nameCate);
+        imageUpdate = addMenu.findViewById(R.id.imageUpdate);
+
+        edtName.setText(cate.getName());
+
+        fSelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermission();
+            }
+        });
+
+        fUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateCategory(cate);
+            }
+        });
+
+        dialog.setView(addMenu);
+        dialog.setIcon(R.drawable.icons8_hamburger_100);
+
+        dialog.setPositiveButton("YES" , new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                cate.setGmail(cate.getGmail());
+                cate.setName(String.valueOf(edtName.getText()));
+
+                databaseReference = FirebaseDatabase.getInstance().getReference(Container.CATEGORY);
+                databaseReference.child(key).setValue(cate);
+            }
+        });
+
+        dialog.setNegativeButton("NO" , new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private void updateCategory(Category cate) {
+        ProgressDialog dialogUpload = new ProgressDialog(this);
+        dialogUpload.setMessage("Uploading...");
+        dialogUpload.show();
+
+        Calendar calendar = Calendar.getInstance();
+
+        StorageReference mountainsRef = storageRef.child("image" + calendar.getTimeInMillis()+".png");
+        imageUpdate.setDrawingCacheEnabled(true);
+        imageUpdate.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) imageUpdate.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] data = baos.toByteArray();
+        UploadTask uploadTask = mountainsRef.putBytes(data);
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(HomeActivity.this, "Fail", Toast.LENGTH_LONG).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Task<Uri> result = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+                result.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        cate.setImage(uri.toString());
+                    }
+                });
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double process = ( 100 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                dialogUpload.setMessage("Uploaded"+process+"%");
+                if(process == 100.0d){
+                    dialogUpload.dismiss();
+                }
+            }
+        });
+    }
+
+    private void requestPermission(){
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(HomeActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                openImagePicker();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(HomeActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+        TedPermission.create()
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check();
+    }
+
+    private void openImagePicker(){
+        TedBottomPicker.OnImageSelectedListener imageSelectedListener = new TedBottomPicker.OnImageSelectedListener(){
+            @Override
+            public void onImageSelected(Uri uri) {
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    imageUpdate.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(HomeActivity.this)
+                .setOnImageSelectedListener(imageSelectedListener)
+                .create();
+        tedBottomPicker.show(getSupportFragmentManager());
+    }
+
 }
--- app/src/main/java/com/example/MyCollection/ViewHolder/MenuViewHolder.java
+++ app/src/main/java/com/example/MyCollection/ViewHolder/MenuViewHolder.java
@@ -1,5 +1,6 @@
 package com.example.MyCollection.ViewHolder;
 
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -7,10 +8,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.MyCollection.Common.Container;
 import com.example.MyCollection.Interface.ItemClickListener;
 import com.example.foodnews.R;
 
-public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener{
 
     public static TextView txtMenuTitle;
     public static ImageView imageViiew;
@@ -21,6 +23,7 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnCl
         txtMenuTitle = (TextView) itemView.findViewById(R.id.menu_name);
         imageViiew = (ImageView) itemView.findViewById(R.id.ImageViewMenu);
 
+        itemView.setOnCreateContextMenuListener(this);
         itemView.setOnClickListener(this);
 
     }
@@ -32,6 +35,14 @@ public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     @Override
     public void onClick(View v) {
         itemClickListenerr.onClick(v, getAdapterPosition(),false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("CHOOSE ACTIOM: ");
+
+        menu.add(0,0,getAdapterPosition(), Container.UPDATE);
+        menu.add(0,0,getAdapterPosition(), Container.DELETE);
     }
 
 }
